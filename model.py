41392bb642a304b781f9f5a4707c9e3bd469921a
algometrix <ashishnagar31@gmail.com>
2018-08-10 02:20:58 +0530

    First Commit

---
+++ model.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+# Creating network for the equation z = ax + by
+
+dataset_size = 10
+
+dataset = np.zeros(shape=(dataset_size,dataset_size,1))
+weights = np.zeros(shape=(1,2))
+learning_rate = 0.01
+
+# Generate dataset
+for x in range(0, dataset_size):
+    for y in range(0, dataset_size):
+        a = 2 # z = ax + by
+        b = 3 # z = ax + by
+        z = a*x + b*y
+        dataset[x,y] = z
+        #print('X : {} | Y : {} | Z : {}'.format(x,y,z))
+
+def forward(input, weights):
+    np_input = np.array(input)
+    result = np.matmul(np_input, weights.T)
+    return result
+
+def backpropogate(input, result, target, weights):
+    #print('input:{} | result:{} | target:{} | weights:{}'.format(input, result, target, weights))
+    gradient = result - target
+    #print('Gradient : {}'.format(gradient))
+    update_value = gradient * input
+    #print('Update Value : {}'.format(update_value))
+    return update_value
+
+#weights[0,0] = 1
+#weights[0,1] = 1
+
+print('Weights : {}'.format(weights))
+
+for i in range(0,10):
+    for x in range(0, dataset_size):
+        for y in range(0, dataset_size):
+            print('------------------------------------------------------------------------------------------------')
+            input = [x,y]
+            target = dataset[x,y]
+            result = forward(input, weights)
+            error = ((result - target)**2) / 2
+            weight_delta = backpropogate(input, result, target, weights)
+            weights = weights - (learning_rate * weight_delta)
+            w1 = weights[0,0]
+            w2 = weights[0,1]
+            print("x: {} \t| y: {} \t| target: {} \t| result: {} \t| E: {} \t| W1: {} \t| W2: {}"
+            .format(x,       y,  target,      result,    error,    w1,      w2,))
